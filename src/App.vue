98f6567792cb1521f9849c8aa741ab2c173a8602
Tvxq0728 <pero.kikuchi@gmail.com>
2022-03-28 02:06:46 +0900

    エラー

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-link to="/">ECサイトのトップページ</router-link>
+    <router-link to="/manufacture-list">
+      商品一覧ページ
+    </router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
